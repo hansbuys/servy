184978b4737fd004fb16c4072daab08a2b8224c5
Hans Buys <buyshans@gmail.com>
2018-11-20 22:09:21 -0500

    Added assertion on response body.

--- test/servy_test.exs
+++ test/servy_test.exs
@@ -1,13 +1,15 @@
 defmodule ServyTest do
   use ExUnit.Case
+  require Logger
 
   test "/ returns OK" do
     port = 8082
     server = spawn(fn -> Servy.accept(port) end)
 
     try do
-      {:ok, {{_, status, _}, _, _}} = :httpc.request('http://localhost:#{port}/')
+      {:ok, {{_, status, _}, _, body}} = :httpc.request('http://localhost:#{port}/')
       assert status == 200
+      assert List.to_string(body) =~ "Pistons, Tigers, RedWings"
     after
       Process.delete(server)
     end
